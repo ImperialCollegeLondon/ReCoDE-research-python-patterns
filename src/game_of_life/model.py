8fbd802bba55e4d1611da6490284507a6eb1ba79
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-03-22 13:25:47 +0000

    refactor: Move logic into the grid class
    I realised it might be redundant to separate it out. This also means that I can now combine all the information and get the full time history

--- src/game_of_life/model.py
+++ src/game_of_life/model.py
@@ -10,20 +10,38 @@ class Grid:
     N_BIRTH: ClassVar[int] = 3
     N_SURVIVAL: ClassVar[int] = 2  # assumes that N_BIRTH is also a survival
 
-    def __init__(self, grid_size: int = 50, wrap: bool = True) -> None:
+    def __init__(self, grid_size: int = 50, wrap: bool = True, store_history: bool = False) -> None:
         self.grid_size: int = grid_size
         self.wrap: bool = wrap
-        self._grid: NDArrayInt = np.zeros((grid_size, grid_size), dtype=int)
+        self._generation: int = 0
+        self._grid: NDArrayInt = np.zeros((grid_size, grid_size), dtype=np.uint8)
+        self._history: list[NDArrayInt] | None = [self._grid] if store_history else None
 
     def population(self) -> int:
         return int(self._grid.sum())
 
+    @property
+    def grid(self) -> NDArrayInt:
+        return self._grid
+
+    @property
+    def generation(self) -> int:
+        return self._generation
+
+    def all_grid_history(self) -> NDArrayInt | None:
+        if self._history is not None:
+            return np.dstack(self._history)
+        return self._history
+
     def randomise(self, density: float = 0.2) -> None:
+        assert self.generation == 0, "Grid can only be randomised at the start"
         self._grid = np.random.default_rng().choice(
             [0, 1], size=(self.grid_size, self.grid_size), p=np.asarray([1 - density, density])
         )
+        if self._history is not None:
+            self._history[0] = self._grid
 
-    def step(self) -> None:
+    def compute_next_generation(self) -> NDArrayInt:
         neighbours = (
             np.roll(np.roll(self._grid, 1, axis=0), 1, axis=1)
             + np.roll(np.roll(self._grid, 1, axis=0), -1, axis=1)
@@ -46,18 +64,10 @@ class Grid:
             next_generation[:, 0] = 0
             next_generation[:, -1] = 0
 
-        self._grid = next_generation
-
-
-class GameOfLife:
-    def __init__(self, grid: Grid) -> None:
-        self.generation: int = 0
-        self.grid: Grid = grid
-
-    @property
-    def population(self) -> int:
-        return self.grid.population()
+        return next_generation
 
     def step(self) -> None:
-        self.grid.step()
-        self.generation += 1
+        self._generation += 1
+        self._grid = self.compute_next_generation()
+        if self._history is not None:
+            self._history.append(self._grid)
