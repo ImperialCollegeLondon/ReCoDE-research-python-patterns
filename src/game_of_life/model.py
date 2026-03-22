1658ff18e430788d177b578efaeeef9d54537415
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-03-22 18:37:36 +0000

    refactor: Moved pattern to be above so it can be used for function

--- src/game_of_life/model.py
+++ src/game_of_life/model.py
@@ -8,6 +8,45 @@ from pydantic import BaseModel, PositiveInt, field_validator, model_validator
 NDArrayInt = npt.NDArray[np.uint8]
 
 
+class Pattern(BaseModel):
+    width: PositiveInt
+    height: PositiveInt
+    encoded_pattern: str
+
+    @field_validator("encoded_pattern", mode="after")
+    @classmethod
+    def validate_pattern_string(cls, pattern_str: str) -> str:
+        if not pattern_str.endswith("!"):
+            raise ValueError("Pattern string must end with an '!'")
+
+        without_end: str = pattern_str[:-1]
+
+        core_pattern = r"^(\d*[bo$])*$"
+        if re.match(core_pattern, without_end) is None:
+            raise ValueError("Pattern contains invalid characters or structure")
+
+        # Check that all numbers are > 0
+        pattern_for_counts = r"(\d+)([bo$])"
+        for match in re.finditer(pattern_for_counts, without_end):
+            if int(match.group(1)) == 0:
+                raise ValueError("Run count cannot be 0 at {match.start()}")
+
+        return without_end
+
+    @model_validator(mode="after")
+    def check_height_matches_pattern(self) -> Self:
+        num_new_lines: int = self.height - 1
+        if self.encoded_pattern.count("$") != num_new_lines:
+            # Matches groups of digits with $. Then, sums the digits to get number of new lines for that group
+            new_line_sum: int = sum(
+                int(m.group(1)) if m.group(1).isdecimal() else 1
+                for m in re.finditer(r"(\d*)(\$)", self.encoded_pattern)
+            )
+            if new_line_sum != num_new_lines:
+                raise ValueError("Number of new lines does not match specified pattern height")
+        return self
+
+
 class GridInitialiser(Protocol):
     def __call__(self, n_rows: int, n_cols: int, **kwargs: dict[str, Any]) -> NDArrayInt: ...
 
@@ -92,47 +131,3 @@ class Grid:
         self._grid = self.compute_next_generation()
         if self._history is not None:
             self._history.append(self._grid)
-
-
-class Pattern(BaseModel):
-    width: PositiveInt
-    height: PositiveInt
-    encoded_pattern: str
-
-    @field_validator("encoded_pattern", mode="after")
-    @classmethod
-    def validate_pattern_string(cls, pattern_str: str) -> str:
-        if not pattern_str.endswith("!"):
-            raise ValueError("Pattern string must end with an '!'")
-
-        without_end: str = pattern_str[:-1]
-
-        # valid_pattern = re.compile(r"^[\d$bo]*$")
-        # if not valid_pattern.match(without_end ):
-        #    invalid_chars = "".join(sorted(set(re.findall(r"[^\d$bo]", without_end))))
-        #    raise ValueError(f"Invalid characters found: {invalid_chars}")
-        core_pattern = r"^(\d*[bo$])*$"
-        if re.match(core_pattern, without_end) is None:
-            raise ValueError("Pattern contains invalid characters or structure")
-
-        # Check that all numbers are > 0
-        pattern_for_counts = r"(\d+)([bo$])"
-        for match in re.finditer(pattern_for_counts, without_end):
-            if int(match.group(1)) == 0:
-                raise ValueError("Run count cannot be 0 at {match.start()}")
-
-        # return pattern_str[:-1]
-        return without_end
-
-    @model_validator(mode="after")
-    def check_height_matches_pattern(self) -> Self:
-        num_new_lines: int = self.height - 1
-        if self.encoded_pattern.count("$") != num_new_lines:
-            # Matches groups of digits with $. Then, sums the digits to get number of new lines for that group
-            new_line_sum: int = sum(
-                int(m.group(1)) if m.group(1).isdecimal() else 1
-                for m in re.finditer(r"(\d*)(\$)", self.encoded_pattern)
-            )
-            if new_line_sum != num_new_lines:
-                raise ValueError("Number of new lines does not match specified pattern height")
-        return self
