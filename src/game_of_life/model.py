8e777de9c6859cccc11540d612901f813d153b8c
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-03-26 10:09:03 +0000

    refactor: Moved getting counts out into a utility function

--- src/game_of_life/model.py
+++ src/game_of_life/model.py
@@ -46,14 +46,17 @@ class Pattern(BaseModel):
 
         return without_end
 
+    @staticmethod
+    def get_counts(match: re.Match[str], *, group_num: int = 1) -> int:
+        return int(counts) if (counts := match.group(group_num)) else 1
+
     @model_validator(mode="after")
     def check_height_matches_pattern(self) -> Self:
         num_new_lines: int = self.height - 1
         if self.encoded_pattern.count("$") != num_new_lines:
             # Matches groups of digits with $. Then, sums the digits to get number of new lines for that group
             new_line_sum: int = sum(
-                int(m.group(1)) if m.group(1).isdecimal() else 1
-                for m in re.finditer(r"(\d*)(\$)", self.encoded_pattern)
+                self.get_counts(m, group_num=1) for m in re.finditer(r"(\d*)(\$)", self.encoded_pattern)
             )
             if new_line_sum != num_new_lines:
                 raise ValueError("Number of new lines does not match specified pattern height")
@@ -72,7 +75,7 @@ class Pattern(BaseModel):
             #   2. ([bo]): Matches the characters "b" or "o" which specifies the liveness of a cell
             #              b => dead cell, o => live cell
             for match_in_row in re.finditer(r"(\d*)([bo])", row_pattern):
-                num_to_set: int = int(match) if (match := match_in_row.group(1)) else 1
+                num_to_set: int = self.get_counts(match_in_row, group_num=1)
 
                 # else case is where match_in_row.group(2) == "b". Thus, the cell value should be set to 0
                 liveness: int = 1 if match_in_row.group(2) == "o" else 0
