47d5b831a89d7a6e88dff926f20c19e581383ea9
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-04-09 13:12:46 +0100

    feat: Introduced interface for View abstraction

--- src/game_of_life/view/base.py
+++ src/game_of_life/view/base.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game_of_life.model import GameOfLife
 
 
 class BaseView(ABC):
     @abstractmethod
-    def render(self) -> None: ...
+    def setup(self) -> None: ...
+    @abstractmethod
+    def render(self, game: "GameOfLife") -> None: ...
+    @abstractmethod
+    def teardown(self) -> None: ...
