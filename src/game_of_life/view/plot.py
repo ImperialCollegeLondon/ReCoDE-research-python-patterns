73d4c54e55e4e2ba3ec37637e563fe13990d7c9b
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-04-12 15:23:41 +0100

    feat: Implemented view using matplotlib

--- src/game_of_life/view/plot.py
+++ src/game_of_life/view/plot.py
@@ -1,8 +1,19 @@
-from typing import TYPE_CHECKING, override
+from pathlib import Path
+from typing import TYPE_CHECKING, ClassVar, override
+
+from matplotlib import animation
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+from rich.progress import track
 
 from game_of_life.view.base import BaseView
 
 if TYPE_CHECKING:
+    from matplotlib.axes import Axes
+    from matplotlib.figure import Figure
+    from matplotlib.image import AxesImage
+    from matplotlib.text import Text
+
     from game_of_life.model import GameOfLife
 
 
@@ -11,14 +22,50 @@ class PlotView(BaseView):
     View interface for displaying/exporting with matplotlib
     """
 
-    def __init__(self) -> None:
+    INTERVAL: ClassVar[int] = 100
+    output_path: Path | None
+    fig: "Figure"
+    ax: "Axes"
+    _cmap: ListedColormap
+    _frame_artists: list[list["AxesImage | Text"]]
+
+    def __init__(self, output_path: Path | None = None) -> None:
         super().__init__()
+        self.output_path = output_path
+        self._cmap = ListedColormap(["white", "black"])
+        self.fig, self.ax = plt.subplots(constrained_layout=True)
+        self._frame_artists = []
 
     @override
-    def setup(self) -> None: ...
+    def setup(self) -> None:
+        self.fig.suptitle("Game of Life")
+
+        # Disable axis ticks
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
 
     @override
-    def render(self, game: "GameOfLife") -> None: ...
+    def render(self, game: "GameOfLife") -> None:
+        self._frame_artists.append([self.ax.imshow(game.grid, cmap=self._cmap, interpolation="nearest")])
 
     @override
-    def teardown(self) -> None: ...
+    def teardown(self) -> None:
+        assert self._frame_artists, "List of artists must not be empty"
+
+        animated = animation.ArtistAnimation(
+            self.fig,
+            self._frame_artists,
+            interval=self.INTERVAL,
+            blit=True,
+            repeat=True,
+        )
+
+        if self.output_path is None:
+            plt.show()
+        else:
+            animated.save(
+                self.output_path,
+                savefig_kwargs={"bbox_inches": "tight"},
+                progress_callback=lambda current, total: track(self._frame_artists, completed=current, total=total),
+            )
+        plt.close(self.fig)
